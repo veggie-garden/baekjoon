0056ddffc5addc9e1cc5d71fc8898890937e6371
veggie-garden <devcseo@gmail.com>
2021-06-27 00:18:31 +0900

    Update hanoi_11729.c

--- recursive/hanoi_11729.c
+++ recursive/hanoi_11729.c
@@ -1,10 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#hanoi 함수 생성
-void hanoi(int n, char a, char b, char c) #판 개수, 기둥 1, 2, 3
+//hanoi 함수 생성
+//판 개수, 기둥 from은 시작하는 기둥, by는 거쳐가는 기둥, to는 최종 도착하는 기둥
+void hanoi(int n, char from, char by, char to)
 {
-	#만약 옮길 수 있는 판이 하나만 남았다면 출력
+	//만약 옮길 수 있는 판이 하나만 남았다면 출력
 	if (n == 1)
 		printf("%c %c\n", a, c);
 	else
@@ -17,11 +18,24 @@ void hanoi(int n, char a, char b, char c) #판 개수, 기둥 1, 2, 3
 
 int main(void)
 {
-	int n, count = 1; #n, 1이 담겨 있는 count 변수 생성
-	scanf("%d", &n); #n 입력받기
-	for (int i = 0; i < n; i++) #이동 횟수 계산
+	int n, count = 1;
+	scanf("%d", &n); 
+	
+	//이동 횟수 계산 
+	for (int i = 0; i < n; i++)
 		count *= 2;
-	printf("%d\n", count - 1); #이동 횟수 출력
+	printf("%d\n", count - 1);
+	/*
+	 * 한 원반을 다른 기둥으로 옮길 때 걸리는 최소 이동 횟수가 2^n - 1 번이다. 
+	 * 그럼 원반이 3개라면 총 2^3 -1 번 이동한다. 
+	 * 그래서 for문으로 2의 제곱을 구한 후, -1을 하는 것이다.
+	 
+	 * 만약 for문으로 쓰기 싫다면 pow라는 함수가 있다. math.h 라는 라이브러리를 불러오면 사용할 수 있다. 
+	 * 사용 예시
+	   int count = pow(2, N)- 1;
+	   print("%d\n", count);
+	*/
+	
 	hanoi(n, '1', '2', '3');
 	return (0);
 }
