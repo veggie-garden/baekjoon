183ef19d8a497283c2363181a089b26ab1c1e1dd
veggie-garden <devcseo@gmail.com>
2021-06-27 14:02:45 +0900

    Update hanoi_11729.c

--- recursive/11729/hanoi_11729.c
+++ recursive/11729/hanoi_11729.c
@@ -4,12 +4,15 @@
 void hanoi(int n, char from, char by, char to)
 {
 	if (n == 1)
-		printf("%c %c\n", a, c);
+  	{
+		printf("%c %c\n", from, to);
+    		return;
+  	}
 	else
 	{
-		hanoi(n - 1, a, c, b);
-		printf("%c %c\n", a, c);
-		hanoi(n - 1, b, a, c);
+		hanoi(n - 1, from, to, by);
+		printf("%c %c\n", from, to);
+		hanoi(n - 1, by, from, to);
 	}
 }
 
@@ -17,11 +20,9 @@ int main(void)
 {
 	int n, count = 1;
 	scanf("%d", &n); 
-	
 	for (int i = 0; i < n; i++)
 		count *= 2;
 	printf("%d\n", count - 1);
-	
 	hanoi(n, '1', '2', '3');
 	return (0);
 }
